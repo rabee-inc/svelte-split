2b903c9e2238d367ec20cc4747af7e62b3ed4cbc
phi <phi1618jp@gmail.com>
2022-04-16 16:52:43 +0900

    fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <template lang='pug'>
-  Split.f.s-full(storageKey='project-editor', defaultSizes='{[30, 40, 30]}')
+  Split.f.s-full(storageKey='demo', defaultSizes='{[30, 40, 30]}')
     div.bg-red.f.fh
       div.text-white.fs32.bold red
     div.bg-green.f.fh
